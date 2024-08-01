f6553167b28528a77413f4b46870da018d36f1de
crazydev940101 <crazydev030905@gmail.com>
2024-08-01 11:28:22 -0400

    Updated review section for podcast

--- public/js/od-review-for-podcast.js
+++ public/js/od-review-for-podcast.js
@@ -2,7 +2,24 @@
     'use strict';
 
     jQuery(document).ready(function ($) {
-        let fieldList = $(this).data('field');
-        console.log(1, fieldList);
+		            
+            // Access the individual data attributes
+            let rating = $('.glsr-field-rating');
+            let content = $('.glsr-field-textarea');
+            let name = $('.glsr-field-text');
+            let email = $('.glsr-field-email'); 
+		
+			rating.find('label').hide();
+			content.find('label').hide();
+		    name.find('label').hide();
+		    email.find('label').hide();
+		
+		    content.find('textarea').attr('placeholder', 'Tell us your thought');
+		
+            console.log('Rating:', rating); // Outputs: "Rating"
+            console.log('Content:', content); // Outputs: "Content"
+            console.log('Name:', name); // Outputs: "Name"
+		    console.log('Email:', email); // Outputs: "Email"
+
     });
 })(jQuery);

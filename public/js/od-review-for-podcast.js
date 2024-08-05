c0c84154aec80bde8c581dc4a0261295d3f4afce
crazydev940101 <crazydev030905@gmail.com>
2024-08-05 13:55:59 -0400

    Updated css and js for podcast

--- public/js/od-review-for-podcast.js
+++ public/js/od-review-for-podcast.js
@@ -2,6 +2,10 @@
     'use strict';
 
     jQuery(document).ready(function ($) {
-		            
+       $('.wpmtst-testimonial').each(function () {
+            if ($(this).find('.readmore-excerpt').length === 0) {
+                $(this).find('.readmore-toggle').hide();
+            }
+        });        
     });
 })(jQuery);

309aa79b67b0b95473e7c7a89711ed18f1c49a38
crazydev940101 <crazydev030905@gmail.com>
2024-08-05 15:17:05 -0400

    Updated css and js

--- public/js/od-review-for-podcast.js
+++ public/js/od-review-for-podcast.js
@@ -2,10 +2,6 @@
     'use strict';
 
     jQuery(document).ready(function ($) {
-       $('.wpmtst-testimonial').each(function () {
-            if ($(this).find('.readmore-excerpt').length === 0) {
-                $(this).find('.readmore-toggle').hide();
-            }
-        });        
+           
     });
 })(jQuery);
